cde35660dde66102591615bb336c7741de6dd502
Richard Mable <richardbmable@gmail.com>
2016-01-15 10:46:54 -0500

    Created two methods in the application controller for setting sessions for a port manager or a salesman, call @currentPortManager or @currentSalesman to access methods for those respecitive classes if they are logged in. Set them to check if logged in on every action. Created two methods no_current_port_manager and no_current_salesman to use if we do not want one or the other accessing a page if they are are not logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,41 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #Check if a port manager or salesman is logged in before every action
+  before_action :current_port_manager
+  before_action :current_salesman
+
+	#this will set the session if a user logs in as 
+	#a Port Manager and call @currentPortManager
+	#to run methods on the logged in user
+	def current_port_manager
+		if session[:user_id]
+			@currentPortManager = PortManger.find(session[:user_id])
+		end
+	end
+
+	#this will set the session if a user
+	#logs in as a Salesman, and call @currentSalesman
+	#to run methods on the logged in user
+	def current_salesman
+		if session[:user_id]
+			@currentSalesman = Salesman.find(session[:user_id])
+		end
+	end
+
+	#this is a method if we do not want
+	#a Port Manager to access a view without being logged in
+	def no_current_port_manager
+		if not @currentPortManager
+			flash[:alert] = "You need to be logged as a Port Manager to do that!"
+		end
+	end
+
+	#this is a method if we do not want
+	#a Salesman to access a view without being logged in
+	def no_current_salesman
+		if not @currentSalesman
+			flash[:alert] = "You need to be logged as a Salesman to do that!"
+		end
+	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,20 @@
 class SessionsController < ApplicationController
-  def create
-  end
 
-  def destroy
-  end
+	#sessions index will be our landing page
+	def index
+
+	end
+
+	#this action will have two different paths
+	#depending on whether the user logs in as port manager
+	#or a salesman
+	def create
+	end
+
+	#this will log a user out and redirect them
+	#to the landing page
+	def destroy
+	end
+
+	
 end

73b4671ddbed2a4a2905939375eebb6c280ee775
Kyle N. Smith <smith.kylen@gmail.com>
2016-01-17 12:38:47 -0500

    added pending workorders to port manager index

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'logout' => 'sessions#destroy', as: :logout
   #set the root page to be the login page
   root 'sessions#index'
+  get 'test' => 'port_managers#test'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
